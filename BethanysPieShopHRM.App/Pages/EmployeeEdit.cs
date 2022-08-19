ff9906acf4eaffb5c156245bbcb71ea529f0e4d1
Lawrence LCI <lcheunglci@gmail.com>
2022-08-19 00:13:20 -0700

    Add Employee Edit form page

---
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+
+    }
+}
