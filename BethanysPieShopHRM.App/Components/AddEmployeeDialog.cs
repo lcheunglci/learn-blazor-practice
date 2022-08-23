73a356398500b48687a94a1643efb157090541c8
Lawrence LCI <lcheunglci@gmail.com>
2022-08-22 23:54:58 -0700

    Add quick add employee component

---
+++ BethanysPieShopHRM.App/Components/AddEmployeeDialog.cs
@@ -0,0 +1,57 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class AddEmployeeDialog
+    {
+        public Employee Employee { get; set; } =
+            new Employee() {
+                CountryId = 1,
+                JobCategoryId = 1,
+                BirthDate = DateTime.Now,
+                JoinedDate = DateTime.Now };
+
+        [Inject]
+        public IEmployeeDataService EmployeDataService { get; set; }
+
+        public bool ShowDialog { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> CloseEventCallback { get; set; }
+
+        public void Show()
+        {
+            ResetDialog();
+            ShowDialog = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            ShowDialog = false;
+            StateHasChanged();
+        }
+
+        private void ResetDialog()
+        {
+            Employee = new Employee()
+            {
+                CountryId = 1,
+                JobCategoryId = 1,
+                BirthDate = DateTime.Now,
+                JoinedDate = DateTime.Now
+            };
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            await EmployeDataService.AddEmployee(Employee);
+            ShowDialog = false;
+
+            await CloseEventCallback.InvokeAsync(true);
+            StateHasChanged();
+        }
+    }
+}
--- BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
@@ -1,4 +1,5 @@
-﻿using BethanysPieShopHRM.App.Services;
+﻿using BethanysPieShopHRM.App.Components;
+using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
@@ -15,11 +16,24 @@ namespace BethanysPieShopHRM.App.Pages
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
 
+        protected AddEmployeeDialog AddEmployeeDialog { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
             //var data = await HttpClient.GetFromJsonAsync<List<Employee>>("https://localhost:44340/api/Employee");
 
             Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+        }
+
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
+        }
+
+        public async void AddEmployeeDialog_OnDialogClose()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+            StateHasChanged();
         }
 
     }
