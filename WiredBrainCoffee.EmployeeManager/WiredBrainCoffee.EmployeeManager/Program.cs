b0baf0129ab4837eecc55dbbc7263dca10745069
Lawrence LCI <lcheunglci@gmail.com>
2023-02-14 22:54:05 -0800

    Add migration at runtime

--- WiredBrainCoffee.EmployeeManager/WiredBrainCoffee.EmployeeManager/Program.cs
+++ WiredBrainCoffee.EmployeeManager/WiredBrainCoffee.EmployeeManager/Program.cs
@@ -12,6 +12,19 @@ builder.Services.AddDbContext<EmployeeManagerDbContext>(opt => opt.UseSqlServer(
 
 var app = builder.Build();
 
+// Don't do this in production, just useful for development
+await EnsureDatabaseIsMigrated(app.Services);
+async Task EnsureDatabaseIsMigrated(IServiceProvider services)
+{
+    using var scope = services.CreateScope();
+    using var ctx = scope.ServiceProvider.GetService<EmployeeManagerDbContext>();
+    if (ctx is not null)
+    {
+        await ctx.Database.MigrateAsync();
+    }
+}
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
